a0a0287ee55a7764b2371348488d74ecde2e45f3
Caio Oliveira <xcaio2@gmail.com>
2023-10-09 15:14:44 +0100

    :)

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ocaio-re <ocaio-re@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 19:17:29 by ocaio-re          #+#    #+#             */
-/*   Updated: 2023/10/09 14:51:31 by ocaio-re         ###   ########.fr       */
+/*   Updated: 2023/10/09 15:13:41 by ocaio-re         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,28 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int	ft_isascii(int c);
-int ft_isprint(int c);
-int	ft_strlen(const char *str);
-void	*ft_memset(void *b, int c, size_t len);
-void    ft_bzero(void *s, size_t n);
-void    *ft_memcpy(void *dst, const void *src, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(const char *str);
+void	ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *s1, const void *s2, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char    ft_toupper(char c);
-char    ft_tolower(char c);
+char	ft_toupper(char c);
+char	ft_tolower(char c);
 char	*ft_strchr(const char *s, int i);
 char	*ft_strrchr(const char *s, int c);
-int	ft_strncmp(char *s1, char *s2, unsigned int n);
+int		ft_strncmp(char *s1, char *s2, unsigned int n);
 void	*ft_memchr(const void *s, int c, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-int	ft_atoi(const char *nptr);
+int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t count, size_t size);
-char    *ft_strdup(char *src);
+char	*ft_strdup(const char *s1);
 
 #endif
