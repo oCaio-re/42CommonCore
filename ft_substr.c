b9ee613bfb2435111ae9d26a888c04f211b0d6fc
Caio <xcaio2@gmail.com>
2023-10-10 12:43:05 +0100

    ---

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,13 @@
 /*   By: caioba <caioba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 12:41:36 by caioba            #+#    #+#             */
-/*   Updated: 2023/10/10 12:42:15 by caioba           ###   ########.fr       */
+/*   Updated: 2023/10/10 12:42:58 by caioba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	size_t	j;
